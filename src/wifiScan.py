df1751d5880eb5fa6721a515633561f074d99dbc
qiuyu <qiuyu1005@163.com>
2020-03-23 01:13:33 -0400

    algorithm

--- src/wifiScan.py
+++ src/wifiScan.py
@@ -1,6 +1,8 @@
 import pywifi
 import time
 import pandas as pd
+import re
+import math
 
 AUTH_TYPES = ['OPEN', 'SHARED']
 KEY_TYPES = ['NONE', 'WPA', 'WPAPSK', 'WPA2', 'WPA2PSK', 'UNKNOWN']
@@ -33,24 +35,37 @@ def scanWifi():
             continue
 
         wifi = []
+        #get name
         name = profile.ssid
         wifi.append(name)
+
+        #get mac
         mac = profile.bssid
-        wifi.append(mac)
+        mac = re.sub(':', '', mac)
+        mac_int = int(mac, 16)
+        wifi.append(mac_int)
+
+        #get auth_types
         auth = AUTH_TYPES[profile.auth[0]]
         wifi.append(auth)
+
+        #get key_types
         type = KEY_TYPES[profile.akm[0]]
         wifi.append(type)
+
+        #get signal
         signal = str(profile.signal)
         wifi.append(signal)
 
         wifilist.append(wifi)
+        #print(wifi)
 
-        #list = wifilist
-        #df = pd.DataFrame(list, columns=["ssid", "bssid", "auth_types", "key_types", "signal"])
-        #df.to_csv(r"C:\Users\asus\Desktop\6221Project\data\test003", index=False)
+        list = wifilist
+        df = pd.DataFrame(list, columns=["ssid", "bssid", "auth_types", "key_types", "signal"])
+        df.to_csv(r"C:\Users\asus\Desktop\6221Project\data\test004", index=False)
 
     return wifilist
+
 
 scanWifi()
 #print(wifilist)
